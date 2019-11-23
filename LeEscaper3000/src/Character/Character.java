13d050405375f7a14c065e1d4ceeeb958d830403
YohannPoissonnier <yohann.poissonnier@gmail.com>
2019-11-23 13:23:11 -0500

    Creation d'une classe pour le personnage et integration dans le classe jeu

---
+++ LeEscaper3000/src/Character/Character.java
@@ -0,0 +1,118 @@
+package Character;
+import org.newdawn.slick.*;
+
+enum Direction{
+	idleRIGHT, idleLEFT, RIGHT, LEFT, JUMPRIGHT, JUMPLEFT
+}
+
+public class Character {
+	private int img_caseX;
+	private int img_caseY;
+	private Image toast;
+	private Animation idle_LEFT;
+	private Animation idle_RIGHT;
+	private Animation anim_RIGHT;
+	private Animation anim_LEFT;
+	private Animation jump_RIGHT;
+	private Animation jump_LEFT;
+	private Direction direction;
+	private Direction AncienneDirection;
+	
+	public Character() {
+		super();
+	}
+	
+	public void renderCharacter(GameContainer gc, Graphics grphcs) {
+		int posX = img_caseX;
+		int posY = img_caseY;
+		
+		switch(direction) {
+		case RIGHT: anim_RIGHT.draw(posX, posY);
+			break;
+		case LEFT: anim_LEFT.draw(posX, posY);
+			break;
+		case idleLEFT: idle_LEFT.draw(posX, posY);
+			break;
+		case idleRIGHT: idle_RIGHT.draw(posX, posY);
+		break;
+		case JUMPRIGHT: jump_RIGHT.draw(posX, posY);
+			break;
+		case JUMPLEFT: jump_LEFT.draw(posX, posY);
+			break;
+		}
+	}
+	public void initCharacter(GameContainer gc) {
+		try {
+			toast = new Image("./sprites/toast.png");
+		} catch (SlickException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		img_caseX = 25;
+		img_caseY = 300;
+		idle_LEFT = getAnimation(0, 2, 0);
+		idle_RIGHT = getAnimation(9, 11, 4);
+		anim_RIGHT = getAnimation(7, 11 ,5);
+		anim_LEFT = getAnimation(0, 4, 1);
+		jump_RIGHT = getAnimation(3, 11, 6);
+		jump_LEFT = getAnimationJumpLeft(7, 1, 2);
+		direction = Direction.LEFT;
+		AncienneDirection = direction;
+	}
+	private Animation getAnimation(int dep, int max, int rowY) {
+		Animation anim = new Animation(false);
+		for(int x = dep; x < max; x++) {
+			anim.addFrame(toast.getSubImage(x*64, rowY*64, 64, 64), 50);
+		}
+		return anim;
+	}
+	private Animation getAnimationJumpLeft(int dep, int max, int rowY) {
+		Animation anim = new Animation(false);
+		for(int x = dep; x > max; x--) {
+			anim.addFrame(toast.getSubImage(x*64, rowY*64, 64, 64), 50);
+		}
+		return anim;
+	}
+	public void updateCharacter(GameContainer gc, int i) {
+		Input input = gc.getInput();
+		//img_caseY += 7;
+		if(input.isKeyDown(Input.KEY_A)) {
+			direction = Direction.LEFT;
+			img_caseX -= 2;
+			anim_LEFT.update((long) (i/2.5));
+			AncienneDirection = direction;
+		}
+		else {
+			if(AncienneDirection == Direction.LEFT) {
+				direction = Direction.idleLEFT;
+				idle_LEFT.update((long) (i/2.5));
+			}
+		}
+		if(input.isKeyDown(Input.KEY_D)) {
+			direction = Direction.RIGHT;
+			img_caseX += 2;
+			anim_RIGHT.update((long) (i/2.5));
+			AncienneDirection = direction;
+		}
+		else {
+			if(AncienneDirection == Direction.RIGHT) {
+				direction = Direction.idleRIGHT;
+				idle_RIGHT.update((long) (i/2.5));
+			}
+		}
+		if(input.isKeyDown(Input.KEY_SPACE)) {
+			if(AncienneDirection == Direction.LEFT) {
+				direction = Direction.JUMPLEFT;
+				img_caseY -= 5;
+				jump_LEFT.update((long) (i/2.5));
+				img_caseY += 5;
+			}
+			else if(AncienneDirection == Direction.RIGHT) {
+				direction = Direction.JUMPRIGHT;
+				img_caseY -= 5;
+				jump_RIGHT.update((long) (i/2.5));
+				img_caseY += 5;
+			}
+		}
+	}
+}
--- LeEscaper3000/src/Jeu.java
+++ LeEscaper3000/src/Jeu.java
@@ -1,146 +1,27 @@
 import org.newdawn.slick.*;
 import org.newdawn.slick.tiled.TiledMap;
 
-enum Direction{
-	idleRIGHT, idleLEFT, RIGHT, LEFT, JUMPRIGHT, JUMPLEFT
-}
+import Character.Character;
 
 public class Jeu extends BasicGame{
-	private int img_caseX;
-	private int img_caseY;
-	private Image toast;
-	private Animation idle_LEFT;
-	private Animation idle_RIGHT;
-	private Animation anim_RIGHT;
-	private Animation anim_LEFT;
-	private Animation jump_RIGHT;
-	private Animation jump_LEFT;
-	private Direction direction;
-	private Direction AncienneDirection;
+	private Character personnage;
 	
 	public Jeu(String title) {
 		super(title);
+		personnage = new Character();
 	}
 
 	public void render(GameContainer gc, Graphics grphcs) throws SlickException {
-		int posX = img_caseX;
-		int posY = img_caseY;
-		
-		switch(direction) {
-		case RIGHT: anim_RIGHT.draw(posX, posY);
-			break;
-		case LEFT: anim_LEFT.draw(posX, posY);
-			break;
-		case idleLEFT: idle_LEFT.draw(posX, posY);
-			break;
-		case idleRIGHT: idle_RIGHT.draw(posX, posY);
-		break;
-		case JUMPRIGHT: jump_RIGHT.draw(posX, posY);
-			break;
-		case JUMPLEFT: jump_LEFT.draw(posX, posY);
-			break;
-		}
+		personnage.renderCharacter(gc, grphcs);
 		
 	}
 
 	public void init(GameContainer gc) throws SlickException {
-		toast = new Image("./sprites/toast.png");
-		img_caseX = 25;
-		img_caseY = 300;
-		idle_LEFT = getAnimationidle_LEFT();
-		idle_RIGHT = getAnimationIdle_Right();
-		anim_RIGHT = getAnimationRight();
-		anim_LEFT = getAnimationLeft();
-		jump_RIGHT = getAnimationJumpRight();
-		jump_LEFT = getAnimationJumpLeft();
-		direction = Direction.LEFT;
-		AncienneDirection = direction;
+		personnage.initCharacter(gc);
 	}
-	private Animation getAnimationJumpRight() {
-		Animation anim = new Animation(false);
-		for(int x = 3; x < 11; x++) {
-			anim.addFrame(toast.getSubImage(x*64, 6*64, 64, 64), 50);
-		}
-		return anim;
-	}
-	private Animation getAnimationJumpLeft() {
-		Animation anim = new Animation(false);
-		for(int x = 7; x > 1; x--) {
-			anim.addFrame(toast.getSubImage(x*64, 2*64, 64, 64), 50);
-		}
-		return anim;
-	}
-	private Animation getAnimationidle_LEFT() {
-		Animation anim = new Animation(false);
-		for(int x = 0; x < 2; x++) {
-			anim.addFrame(toast.getSubImage(x*64, 0*64, 64, 64), 50);
-		}
-		return anim;
-	}
-	private Animation getAnimationRight() {
-		Animation anim = new Animation(false);
-		for(int x = 7; x < 11; x++) {
-			anim.addFrame(toast.getSubImage(x*64, 5*64, 64, 64), 50);
-		}
-		return anim;
-	}
-	private Animation getAnimationLeft() {
-		Animation anim = new Animation(false);
-		for(int x = 0; x < 4; x++) {
-			anim.addFrame(toast.getSubImage(x*64, 1*64, 64, 64), 50);
-		}
-		return anim;
-	}
-	private Animation getAnimationIdle_Right() {
-		Animation anim = new Animation(false);
-		for(int x = 9; x < 11; x++) {
-			anim.addFrame(toast.getSubImage(x*64, 4*64, 64, 64), 50);
-		}
-		return anim;
-	}
+	
 	public void update(GameContainer gc, int i) throws SlickException {
-		Input input = gc.getInput();
-		//img_caseY += 7;
-		if(input.isKeyDown(Input.KEY_A)) {
-			direction = Direction.LEFT;
-			img_caseX -= 2;
-			anim_LEFT.update((long) (i/2.5));
-			AncienneDirection = direction;
-		}
-		else {
-			if(AncienneDirection == Direction.LEFT) {
-				direction = Direction.idleLEFT;
-				idle_LEFT.update((long) (i/2.5));
-			}
-		}
-		if(input.isKeyDown(Input.KEY_D)) {
-			direction = Direction.RIGHT;
-			img_caseX += 2;
-			anim_RIGHT.update((long) (i/2.5));
-			AncienneDirection = direction;
-		}
-		else {
-			if(AncienneDirection == Direction.RIGHT) {
-				direction = Direction.idleRIGHT;
-				idle_RIGHT.update((long) (i/2.5));
-			}
-		}
-		if(input.isKeyDown(Input.KEY_SPACE)) {
-			if(AncienneDirection == Direction.LEFT) {
-				direction = Direction.JUMPLEFT;
-				img_caseY -= 5;
-				jump_LEFT.update((long) (i/2.5));
-				img_caseY += 5;
-			}
-			else if(AncienneDirection == Direction.RIGHT) {
-				direction = Direction.JUMPRIGHT;
-				img_caseY -= 5;
-				jump_RIGHT.update((long) (i/2.5));
-				img_caseY += 5;
-			}
-		}
-		
-				
+		personnage.updateCharacter(gc, i);				
 	}
 
 }
