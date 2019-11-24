d9761d3f000a37b44cbbdcac406171bc1ccf3fa8
Clément <49496407+Clement-Leq@users.noreply.github.com>
2019-11-24 18:38:45 -0500

    Fix bug map et affichage

--- LeEscaper3000/src/Jeu.java
+++ LeEscaper3000/src/Jeu.java
@@ -2,25 +2,31 @@ import org.newdawn.slick.*;
 import org.newdawn.slick.tiled.TiledMap;
 
 import Character.Character;
+import Maps.Maps;
+
 
 public class Jeu extends BasicGame{
 	private Character personnage;
+	private TiledMap map;
+	private int tilesSize;
 	
 	public Jeu(String title) {
 		super(title);
 		personnage = new Character();
 	}
 
-	public void render(GameContainer gc, Graphics grphcs) throws SlickException {
+	public void render(GameContainer gc, Graphics grphcs){
+		map.render(0,0);
 		personnage.renderCharacter(gc, grphcs);
-		
 	}
 
-	public void init(GameContainer gc) throws SlickException {
+	public void init(GameContainer gc) throws SlickException{
+		map = new TiledMap("./maps/Java_Projet.tmx");
+		tilesSize = 32;
 		personnage.initCharacter(gc);
 	}
 	
-	public void update(GameContainer gc, int i) throws SlickException {
+	public void update(GameContainer gc, int i){
 		personnage.updateCharacter(gc, i);				
 	}
 
--- LeEscaper3000/src/Maps/Maps.java
+++ LeEscaper3000/src/Maps/Maps.java
@@ -1,5 +1,36 @@
 package Maps;
 
-public class Maps {
+import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.tiled.TiledMap;
 
+public class Maps extends BasicGame{
+	
+	private TiledMap map;
+	private int tilesSize;
+	
+	public Maps(String title) {
+		super(title);
+		// TODO Auto-generated constructor stub
+	}
+	
+	@Override
+	public void render(GameContainer arg0, Graphics arg1) throws SlickException {
+		map.render(0,0);
+		
+	}
+	
+	@Override
+	public void init(GameContainer arg0) throws SlickException {
+		map = new TiledMap("./maps/Java_Projet.tmx");
+		tilesSize = 32;
+	}
+	
+	@Override
+	public void update(GameContainer arg0, int arg1) throws SlickException {
+		// TODO Auto-generated method stub
+		
+	}
 }
