d53ce0ad41adcba696408d656c772255918c2e13
YohannPoissonnier <yohann.poissonnier@gmail.com>
2019-11-25 11:01:05 -0500

    Fusion des deux travaux

---
+++ LeEscaper3000/src/Character/Gardien.java
@@ -0,0 +1,60 @@
+package Character;
+import org.newdawn.slick.*;
+
+enum Direction2{
+	idleRIGHT, idleLEFT, RIGHT, LEFT, JUMPRIGHT, JUMPLEFT
+}
+
+public class Gardien {
+	private int img_caseX;
+	private int img_caseY;
+	private Image gardien;
+	private Animation anim_muscles;
+	private Animation idle_LEFT;
+	private Animation idle_RIGHT;
+	private Animation anim_RIGHT;
+	private Animation anim_LEFT;
+	private Animation jump_RIGHT;
+	private Animation jump_LEFT;
+	private Direction direction;
+	private Direction AncienneDirection;
+	
+	public Gardien() {
+		super();
+	}	
+	
+	public void renderGardien(GameContainer gc, Graphics grphcs) {
+		int posX = img_caseX;
+		int posY = img_caseY;
+		anim_muscles.draw(9 * 32, 11 * 32);
+	}
+	public void initGardien(GameContainer gc){
+		try {
+			gardien = new Image("./sprites/Police_mur_gros(1).png");
+		} catch (SlickException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		img_caseX = 150;
+		img_caseY = 300;
+		anim_muscles = getAnimation(0, 27, 0);
+		direction = Direction.LEFT;
+		AncienneDirection = direction;
+	}
+	private Animation getAnimation(int dep, int max, int rowY) {
+		Animation anim = new Animation(false);
+		for(int x = dep; x < max; x++) {
+			anim.addFrame(gardien.getSubImage(x*66, rowY*66, 70, 95), 150);
+		}
+		for(int x = max; x > dep; x--) {
+			anim.addFrame(gardien.getSubImage(x*66, rowY*66, 70, 95), 150);
+		}
+		return anim;
+	}
+	public void updateGardien(GameContainer gc, int i) {
+		Input input = gc.getInput();
+		anim_muscles.update(i);
+		
+	}
+}
--- LeEscaper3000/src/Jeu.java
+++ LeEscaper3000/src/Jeu.java
@@ -2,31 +2,36 @@ import org.newdawn.slick.*;
 import org.newdawn.slick.tiled.TiledMap;
 
 import Character.Character;
+import Character.Gardien;
 import Maps.Maps;
 
 
 public class Jeu extends BasicGame{
 	private Character personnage;
 	private Maps map;
+	private Gardien mechantMur;
 	
 	public Jeu(String title) {
 		super(title);
-		personnage = new Character();
 		map = new Maps();
+		mechantMur = new Gardien();
+		personnage = new Character();
 	}
 
 	public void render(GameContainer gc, Graphics grphcs){
 		map.renderMap();
-		
+		mechantMur.renderGardien(gc, grphcs);
 		personnage.renderCharacter(gc, grphcs);
 	}
 
 	public void init(GameContainer gc){
 		map.iniMap();
+		mechantMur.initGardien(gc);
 		personnage.initCharacter(gc);
 	}
 	
 	public void update(GameContainer gc, int i){
+		mechantMur.updateGardien(gc, i);
 		personnage.updateCharacter(gc, i, map);
 		if(map.isGrounded(personnage.getImg_caseX(), personnage.getImg_caseY()+64, "Sol")) {
 			personnage.setImg_caseY(personnage.getImg_caseY()+4);
